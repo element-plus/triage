aac32191b2ba25cbe33dc2582d2e6d78190da13f
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-05-14 11:38:19 +0800

    feat(core): separate functionalities
    - Add auto assign
    - Add flags for controlling each actions
    - Add typing for Triag Config

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,4 +1,11 @@
-export interface TriageConfig {
-  firstTimeGreetings?: string
-  firstTimeLabels?: string[]
+declare interface TriageConfig {
+  autoAssignWhenPRCreated?: boolean // Should auto assign the issue creator as assignees.
+  COCUrl?: string // default to github.com/:owner/:repo/CODE_OF_CONDUCT.md
+  firstTimePRGreetings?: string // first time pull request greetings, exclusive to pRGreetings
+  firstTimePRLabels?: string[] // labels auto attached when first time pull request.
+  labelsWhenPROpened?: string[] // labels auto attached when PR is opened.
+  PRAutoAssignees?: [] // IDs of those needs to be assigned automatically, e.g. PM, PO etc.
+  PRGreetings?: string // pull request greetings, exclusive to firstTimePRGreetings.
+  guidelineUrl?: string // default to github.com/:owner/:repo/README.md
+  skipCollaboratorCheck?: boolean
 }
